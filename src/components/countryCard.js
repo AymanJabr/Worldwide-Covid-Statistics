a15be9549921603b3f44941a5e4ce9fe38aa3d44
ayman <aymanjaber2012@gmail.com>
2021-06-15 13:10:15 +0300

    Add countryCard component

---
+++ src/components/countryCard.js
@@ -0,0 +1,18 @@
+
+
+
+const CountryCard = () => {
+
+
+    return (
+        <div className="countryCard">
+            <img className="countryCardImage"/>
+            <h1 className="countryCardName">Name of Country</h1>
+            <p className="countryCardCasesNumber">N. of Cases</p>
+        </div>
+    )
+
+}
+
+
+export default CountryCard
